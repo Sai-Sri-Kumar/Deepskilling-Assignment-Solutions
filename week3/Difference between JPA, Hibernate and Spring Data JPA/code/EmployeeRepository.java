b0c3f6944365dd49e594353684a515557ccae1f2
Sai-Sri-Kumar <saisrikumar940@gmail.com>
2025-07-04 20:21:20 +0530

    Delete week3/Difference between JPA, Hibernate and Spring Data JPA/code directory

--- week3/Difference between JPA, Hibernate and Spring Data JPA/code/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.orm_learn;
-
-import com.cognizant.orm_learn.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
